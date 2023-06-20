ea6f71d234a75a374badc33ede985ead42c5de38
= <josevictorcomrc@gmail.com>
2023-06-20 12:16:55 -0300

    Unifying branchs Felipe and Testes

--- Saida.java
+++ Saida.java
@@ -1,22 +1,17 @@
-public class Saida{ 
+import java.util.Scanner;public class Saida{ 
 	public static void main(String[] args){
-	String x = "teste";
-	double z = 5.1;
-	int c = 1;
-	if(c>4){
-	System.out.println("C maior que 4");
-	}else{
-	System.out.println("C menor que 4");
-	}
-	while(c<2){
-	System.out.println("C maior que 2");
-	c =  c+1;
-	System.out.println(c);
-	}c =  c+5*2+15*38;
-	z =  c+5*2+25*38-100.10;
+	double  z = 5.1;
+	int  c = 1;
+	int  i = 0;
 	System.out.println("besta");
 	System.out.println(c);
 	System.out.println(z);
+	System.out.println("DEU CERTO PORRAAAAAAAAAAA");
+	do{System.out.println(i);
+	i++;}while(i<5);while(c<4){
+	System.out.println(c);
+	c++;
+	}
 	
 	}
 }
